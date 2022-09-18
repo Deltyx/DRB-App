4f78358ebc49197f3ab4cfcc78fbb1181a641449
Anthony Lammens <anthony.lammens@outlook.com>
2022-09-18 17:35:00 +0200

    Moving drb-app-signalr-tester test page to drb-app-backend project

--- drb-app-backend/drb-app-backend/Program.cs
+++ drb-app-backend/drb-app-backend/Program.cs
@@ -2,40 +2,26 @@ using Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", p =>
-    {
-        p.AllowAnyHeader()
-               .AllowAnyMethod()
-               .SetIsOriginAllowed((host) => true)
-               .AllowCredentials();
-    });
-});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseCors("AllowAll");
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+// TODO : only show Razor page on DEBUG
+app.MapRazorPages();
 app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
