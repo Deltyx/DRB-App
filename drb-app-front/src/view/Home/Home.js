46791e6a30d283fce2cdad2f19f30b77ad441c0a
Alexis <alexismorin.pro@gmail.com>
2022-10-10 01:22:16 +0200

    Home view CSS

--- drb-app-front/src/view/Home/Home.js
+++ drb-app-front/src/view/Home/Home.js
@@ -1,11 +1,17 @@
 import UILobbyManager from '../../components/UILobbyManager/UILobbyManager';
 
+import DRB_LOGO from '../../img/drb-icon-temp.svg';
+
 import './Home.scss';
 
 export default function Home() {
     return (
         <section className='Home-container'>
-            <UILobbyManager />
+            <img className='Home-logo' src={DRB_LOGO} alt={DRB_LOGO} />
+            <h1 className='Home-title'>Draw, Redraw & Bullshitterry</h1>
+            <article className='Home_to_lobby-wrapper'>
+                <UILobbyManager />
+            </article>
         </section>
     );
 }
